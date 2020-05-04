b904ee26c2bf5e377603e0d285223b199fd8e4d5
SoonMinKwun <jaeyoung0509@naver.com>
2020-05-04 10:57:31 +0900

    ㅇㅇㅇㅇ

---
+++ app/src/main/java/com/example/lob/Service/Retrofit.java
@@ -0,0 +1,11 @@
+package com.example.lob.Service;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Retrofit {
+    @SerializedName("")
+    @Expose
+    private String asd;
+    
+}
