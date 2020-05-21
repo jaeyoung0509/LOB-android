a5dd655f47f5d6d23d11bcad8ed89b24292076f3
o3od3 <anysst2@naver.com>
2020-05-21 13:14:51 +0900

    ++++5/21수정

--- app/src/main/java/com/example/lob/UI/board/BoardFragment.java
+++ app/src/main/java/com/example/lob/UI/board/BoardFragment.java
@@ -1,9 +1,17 @@
 package com.example.lob.UI.board;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,24 +22,63 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.lob.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BoardFragment extends Fragment {
 
     private BoardViewModel boardViewModel;
-
+    private ListView boardListView;
+    private BoardListAdapter Adapter;
+    private List<Board> boardList;
+    private ScrollView scrollView;
+    private boolean lastItemVisibleFlag = false;    // 리스트 스크롤이 마지막 셀(맨 바닥)로 이동했는지 체크할 변수
+    private int page = 0;                           // 페이징변수. 초기 값은 0 이다.
+    private final int OFFSET = 5;                  // 한 페이지마다 로드할 데이터 갯수.
+    private boolean mLockListView = false;          // 데이터 불러올때 중복안되게 하기위한 변수
+    private ProgressBar progressBar;                // 데이터 로딩중을 표시할 프로그레스바
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         boardViewModel =
                 ViewModelProviders.of(this).get(BoardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_board, container, false);
-        final TextView textView = root.findViewById(R.id.text_board);
+        boardListView = root.findViewById(R.id.boardListView);
+        boardList = new ArrayList<>();
+        scrollView = root.findViewById(R.id.scrollview_board);
+
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=sdafs-20"));
+        boardList.add(new Board("공지사항입니다.","asdafd","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        boardList.add(new Board("공지사항입니다.","배현지","200=05-20"));
+        Adapter = new BoardListAdapter(getContext(),boardList);
+       // Adapter = new BoardListAdapter(getApplicationContext(),boardList);
+        boardListView.setAdapter(Adapter);
+
+        Button button = root.findViewById(R.id.board_Button); /*페이지 전환버튼*/
+
+        boardListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                scrollView.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
         boardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
+
+
         return root;
     }
-
 }
