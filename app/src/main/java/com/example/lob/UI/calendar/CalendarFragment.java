2b93ba3c0d4802386df1fa785f321ff81f4c2148
jaeyoung0509 <jaeyoung0509@naver.com>
2020-09-29 14:00:48 +0900

    달력 그림그리기

--- app/src/main/java/com/example/lob/UI/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/lob/UI/calendar/CalendarFragment.java
@@ -15,6 +15,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.lob.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
 
@@ -26,12 +31,19 @@ public class CalendarFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         mViewModel =
                 ViewModelProviders.of(this).get(CalendarViewModel.class);
-        View root = inflater.inflate(R.layout.calendar_fragment, container, false);
-        final TextView textView = root.findViewById(R.id.text_calendar);
+        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+      final   MaterialCalendarView cal_food =  root.findViewById(R.id.cal_food);
         mViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                cal_food.state().edit()
+                        .setFirstDayOfWeek(Calendar.SUNDAY)
+                        .setMinimumDate(CalendarDay.from(1990,0,1))
+                        .setMaximumDate(CalendarDay.from(2050,0,1))
+                        .setCalendarDisplayMode(CalendarMode.MONTHS)
+                        .commit();
+
+
             }
         });
         return root;
